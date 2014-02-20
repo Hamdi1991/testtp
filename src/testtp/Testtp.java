110c082196284e220d6707c898fa93ed490b0e7e
USER <USER@TOSH.lan>
2014-02-20 10:36:29 +0100

    Je suis entrain de tester le tuto

---
+++ src/testtp/Testtp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testtp;
+
+/**
+ *
+ * @author USER
+ */
+public class Testtp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello");
+    }
+}
